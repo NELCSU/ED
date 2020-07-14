85b07a4b841988872bce61b3d41e7c7a451391bf
ibuckner <ibuckner@users.noreply.github.com>
2020-07-14 08:47:30 +0100

    Switch to pnpm package manager

--- src/ui/environment.ts
+++ src/ui/environment.ts
@@ -5,11 +5,4 @@ import type { TConfig } from "../typings/ED";
  */
 export function initEnvironment(config: TConfig) {
   config.environment = window.location.hostname === "localhost" ? "DEVELOPMENT" : "PRODUCTION";
-
-  if (config.environment === "DEVELOPMENT") {
-    const dev = document.createElement("div");
-    dev.classList.add("dev-mode");
-    dev.textContent = config.environment;
-    document.body.appendChild(dev);
-	}
 }
